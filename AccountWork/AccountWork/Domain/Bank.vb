493465f499871997e3b1b28d566c90b57296e67e
Henrik Almer <henrik.almer@gmail.com>
2015-09-28 17:01:26 +0200

    Remove redundant column, make interval-end optional and explicitly define the PK to auto increment in all DBRMSs

--- AccountWork/AccountWork/Domain/Bank.vb
+++ AccountWork/AccountWork/Domain/Bank.vb
@@ -1,12 +1,14 @@
-﻿Namespace Domain
+﻿Imports System.ComponentModel.DataAnnotations.Schema
+
+Namespace Domain
     Public Class Bank
         Private _Id As Int32
         Public Property Name As String
-        Public Property ClearingNumber As Int32
-        Public Property ClearingNumberIntervalStart As Int32
-        Public Property ClearingNumberIntervalEnd As Int32
+        Public Property ClearingNumberIntervalStart As Integer
+        Public Property ClearingNumberIntervalEnd As Integer?
         Public Property Email As String
 
+        <DatabaseGenerated(DatabaseGeneratedOption.Identity)>
         Public Property Id() As Int32
             Get
                 Return _Id
