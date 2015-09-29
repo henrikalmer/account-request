53bc2208c46bd9dea753e3ceede0a8932e361828
Henrik Almer <henrik.almer@gmail.com>
2015-09-29 13:49:37 +0200

    Implement search on text box change for clearing numbers.

--- AccountWork/AccountWork/Controls/BankFinder.xaml.vb
+++ AccountWork/AccountWork/Controls/BankFinder.xaml.vb
@@ -1,3 +1,35 @@
-﻿Public Class BankFinder
+﻿Imports AccountWork.Domain
 
+Public Class BankFinder
+    Private Sub clearingTextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
+        Dim textBox As TextBox = sender
+
+        Dim number As Integer
+        If (textBox.Text.Length = 4 And Integer.TryParse(textBox.Text, number)) Then
+            Using Db = New AccountWorkDbContext()
+                Dim Query = From X In Db.ClearingNumbers
+                            Order By X.Name
+                            Select X
+                            Where X.ClearingNumberIntervalStart = number _
+                                And X.ClearingNumberIntervalEnd Is Nothing
+                Dim Item As ClearingNumber = Query.SingleOrDefault()
+
+                If (Item Is Nothing) Then
+                    Query = From X In Db.ClearingNumbers
+                            Order By X.Name
+                            Select X
+                            Where number >= X.ClearingNumberIntervalStart _
+                                And number <= X.ClearingNumberIntervalEnd
+                            Order By X.ClearingNumberIntervalStart Descending
+                    Item = Query.FirstOrDefault()
+                End If
+
+                If (Not Item Is Nothing) Then
+                    bankComboBox.SelectedValue = Item.Name
+                End If
+            End Using
+        Else
+            bankComboBox.SelectedIndex = -1
+        End If
+    End Sub
 End Class
