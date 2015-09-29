a4704ff1b45427919ebe84a6eece7e4b342986cf
Henrik Almer <henrik.almer@gmail.com>
2015-09-29 11:23:41 +0200

    Added custom user control for selecting banks by clearing number. Implemented data binding with a viewmodel.

--- AccountWork/AccountWork/MainWindow.xaml.vb
+++ AccountWork/AccountWork/MainWindow.xaml.vb
@@ -2,26 +2,17 @@
 
 Class MainWindow
     Public Sub New()
-
         ' This call is required by the designer.
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
         Using Db = New AccountWorkDbContext()
-            Dim B = New Bank With {
-                .Name = "Test Bank",
-                .ClearingNumberIntervalStart = 1201,
-                .ClearingNumberIntervalEnd = 1299
-            }
-            Db.Banks.Add(B)
-            Db.SaveChanges()
-
             Dim Query = From X In Db.Banks
                         Order By X.Name
                         Select X
 
             For Each Item As Bank In Query
-                Console.WriteLine(Item.Name)
+                Console.WriteLine(Item.ClearingNumberIntervalStart & ", " & Item.ClearingNumberIntervalEnd & ": " & Item.Name)
             Next
         End Using
     End Sub
