aa4b533446c346d16ac4516020489beffcc653c5
AxelThor76 <Axelthor76@gmail.com>
2015-09-30 10:49:16 +0200

    Begun implementation of word generator

---
+++ AccountWork/AccountWork/Domain/WordDocument.vb
@@ -0,0 +1,33 @@
+﻿Imports word = Microsoft.Office.Interop.Word
+Namespace Domain
+    Public Class WordDocument
+        Public Sub parseGenerateOrder(sPath2template As String, sEBnr As String, sAklname As String, sPnr As String, sName As String, sBankName As String, sClearingno As String, sStartdate As String, sEnddate As String, sType As String)
+            Dim sDomainUser As String = System.Security.Principal.WindowsIdentity.GetCurrent.Name.Replace("\", "/")
+            Dim sADEntry As New DirectoryServices.DirectoryEntry("WinNT://" & sDomainUser)
+            Dim sFullName As String = sADEntry.Properties("FullName").Value
+            Dim oWord As Word.Application
+            oWord = CreateObject("Word.Application")
+            With oWord
+                Stop
+                .Visible = True
+                .Documents.Add("c:\temp\kontobestmall.dotx")
+                .ActiveDocument.Range.Font.Bold = True
+                .Selection.TypeText("Begäran om uppgifter enligt 11 § lag (2004:297) om bank- och finansieringsrörelse")
+                .Selection.TypeParagraph()
+                .Selection.Font.Bold = False
+                .Selection.TypeParagraph()
+                .Selection.TypeParagraph()
+                .Selection.TypeText("I pågående förundersökning " & sEBnr & " begär åklagare att uppgifter enligt 11 § lag (2004:297) om bank- och finansieringsrörelse om enskildes förhållanden lämnas ut enligt följande:")
+                .Selection.TypeParagraph()
+                .Selection.TypeParagraph()
+                If sType = "Engagemangsförfrågan" Then
+                    .Selection.TypeText("Förundersökningen har givit nedanstående ingångsparametrar och vill se om ni, baserat på dessa parametrar kan se om det finns engagemang hos er.")
+                    .Selection.TypeParagraph()
+                    .Selection.TypeParagraph()
+                End If
+            End With
+        End Sub
+    End Class
+End Namespace
+
+
--- AccountWork/AccountWork/MainWindow.xaml.vb
+++ AccountWork/AccountWork/MainWindow.xaml.vb
@@ -1,12 +1,24 @@
 ﻿Imports AccountWork.Domain
 
 Class MainWindow
-    Private Sub checkBox_Click(sender As Object, e As RoutedEventArgs) Handles checkBox.Click
-        Select Case checkBox.IsChecked
+    Private Sub checkBox_Click(sender As Object, e As RoutedEventArgs) Handles chkOrderALL.Checked
+        Select Case chkOrderALL.IsChecked
             Case False
                 bankFinder.Enable()
             Case True
                 bankFinder.Disable()
         End Select
+    End Sub
+
+    Private Sub nameTextBox_TextChanged(sender As Object, e As TextChangedEventArgs) Handles nameTextBox.TextChanged
+
+    End Sub
+
+    Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
+        'Generate word file for order of type
+
+        Dim MailOrderAttachment As New WordDocument
+        MailOrderAttachment.parseGenerateOrder("c:\temp\kontobestmall.dotx", Me.ebNumberTextBox.Text, Me.aklTextBox.Text, Me.pnrTextBox.Text, Me.nameTextBox.Text, Me.nameTextBox.Text, "bank", Me.dateStartDatePicker.Text.ToString, Me.dateEndDatePicker.Text.ToString, Me.tabControl.SelectedItem.ToString)
+
     End Sub
 End Class
