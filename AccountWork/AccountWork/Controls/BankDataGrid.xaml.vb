cd8529abffd31f65b34e13f1ab1344a9af777072
Henrik Almer <henrik.almer@gmail.com>
2015-10-06 10:29:24 +0200

    Moved bank data grid to separate control

---
+++ AccountWork/AccountWork/Controls/BankDataGrid.xaml.vb
@@ -0,0 +1,12 @@
+﻿Public Class BankDataGrid
+    Private Sub dataGridEditButton_Click(sender As Object, e As RoutedEventArgs) Handles dataGridEditButton.Click
+        Select Case clearingNumberDataGrid.IsReadOnly
+            Case True
+                clearingNumberDataGrid.IsReadOnly = False
+                dataGridEditButton.Content = "Lås redigering"
+            Case False
+                clearingNumberDataGrid.IsReadOnly = True
+                dataGridEditButton.Content = "Lås upp för redigering"
+        End Select
+    End Sub
+End Class
