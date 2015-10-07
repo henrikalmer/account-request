0335402c8954ff7910b7800cbcaa82331b99f179
Henrik Almer <henrik.almer@gmail.com>
2015-10-07 14:22:21 +0200

    Make bankfinder accept full account numbers

--- AccountWork/AccountWork/Controls/BankFinder.xaml.vb
+++ AccountWork/AccountWork/Controls/BankFinder.xaml.vb
@@ -18,30 +18,33 @@ Public Class BankFinder
     Private Sub clearingTextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
         Dim textBox As TextBox = sender
 
-        Dim number As Integer
-        If (textBox.Text.Length = 4 And Integer.TryParse(textBox.Text, number)) Then
-            Using Db = New AccountWorkDbContext()
-                Dim Query = From X In Db.ClearingNumbers
-                            Select X
-                            Where X.ClearingNumberIntervalStart = number _
-                                And X.ClearingNumberIntervalEnd Is Nothing
-                Dim Item As ClearingNumber = Query.SingleOrDefault()
+        If (textBox.Text.Length >= 4) Then
+            Dim txt = textBox.Text.Substring(0, 4)
+            Dim number As Integer
+            If (Integer.TryParse(txt, number)) Then
+                Using Db = New AccountWorkDbContext()
+                    Dim Query = From X In Db.ClearingNumbers
+                                Select X
+                                Where X.ClearingNumberIntervalStart = number _
+                                    And X.ClearingNumberIntervalEnd Is Nothing
+                    Dim Item As ClearingNumber = Query.SingleOrDefault()
 
-                If (Item Is Nothing) Then
-                    Query = From X In Db.ClearingNumbers
-                            Select X
-                            Where number >= X.ClearingNumberIntervalStart _
-                                And number <= X.ClearingNumberIntervalEnd
-                            Order By X.ClearingNumberIntervalStart Descending
-                    Item = Query.FirstOrDefault()
-                End If
+                    If (Item Is Nothing) Then
+                        Query = From X In Db.ClearingNumbers
+                                Select X
+                                Where number >= X.ClearingNumberIntervalStart _
+                                    And number <= X.ClearingNumberIntervalEnd
+                                Order By X.ClearingNumberIntervalStart Descending
+                        Item = Query.FirstOrDefault()
+                    End If
 
-                If (Not Item Is Nothing) Then
-                    bankComboBox.SelectedValue = Item.Name
-                End If
-            End Using
-        Else
-            bankComboBox.SelectedIndex = -1
+                    If (Not Item Is Nothing) Then
+                        bankComboBox.SelectedValue = Item.Name
+                    End If
+                End Using
+            Else
+                bankComboBox.SelectedIndex = -1
+            End If
         End If
     End Sub
 End Class
