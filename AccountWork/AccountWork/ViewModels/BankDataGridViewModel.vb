7710e6a0efd23fa09e7cbb9cc8ac9107dfa419a0
Henrik Almer <henrik.almer@gmail.com>
2015-10-07 12:38:45 +0200

    Improvements to bank datagrid view
    Enable adding rows
    Enable deleting rows
    Smart save button
    Reset changes button

--- AccountWork/AccountWork/ViewModels/BankDataGridViewModel.vb
+++ AccountWork/AccountWork/ViewModels/BankDataGridViewModel.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.Collections.ObjectModel
+Imports System.Data.Entity
+Imports System.Data.Entity.Core.Objects.DataClasses
 Imports AccountWork.Domain
 
 Public Class BankDataGridViewModel
@@ -17,6 +19,29 @@ Public Class BankDataGridViewModel
 
     Public Sub New()
         ClearingNumbers = New ObservableCollection(Of ClearingNumber)(Db.ClearingNumbers.ToList())
+    End Sub
+
+    Public Sub Reset()
+        Db.Dispose()
+        ClearingNumbers = Nothing
+        Db = New AccountWorkDbContext()
+        ClearingNumbers = New ObservableCollection(Of ClearingNumber)(Db.ClearingNumbers.ToList())
+        OnPropertyChanged("ClearingNumbers")
+        OnPropertyChanged("HasChanges")
+    End Sub
+
+    Public Sub Add(row As ClearingNumber)
+        ClearingNumbers.Add(row)
+        Db.ClearingNumbers.Add(row)
+        OnPropertyChanged("HasChanges")
+    End Sub
+
+    Public Sub Remove(row As ClearingNumber)
+        ClearingNumbers.Remove(row)
+        If (Not Db.Entry(row).State = EntityState.Detached) Then
+            Db.ClearingNumbers.Remove(row)
+        End If
+        OnPropertyChanged("HasChanges")
     End Sub
 
     Public Sub SaveChanges()
